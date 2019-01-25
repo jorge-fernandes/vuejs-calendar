73e5faccee21d5c10230aa5184ba9f653600fcf9
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-01-25 15:42:05 +0000

    Lesson 127

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,28 @@
 <template>
 	<div>
-		{{ msg }}
+		<div v-for="day in days">{{ day }}</div>
 	</div>
 </template>
 <script>
 	export default {
 		data() {
 			return {
-				msg: 'Hello World!'
+				month: 2,
+				year: 2017
 			}
 		},
-		created() {
-			console.log(this.$moment)
+		computed: {
+			days() {
+				let days = []
+				let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D')
+
+				do {
+					days.push(currentDay)
+					currentDay = this.$moment(currentDay.add(1, 'days'))
+				} while (currentDay.month() + 1 === this.month)
+
+				return days
+			}
 		}
 	}
 </script>
