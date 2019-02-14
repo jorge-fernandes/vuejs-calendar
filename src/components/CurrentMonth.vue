8eb4a55b1649ae2d212083e8878fd2ee18a8bf16
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-02-14 12:20:55 +0000

    Lesson 137 && 138

---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>{{ formattedDate }}</div>
+</template>
+<script>
+	export default {
+		computed: {
+			month() {
+				return this.$store.state.currentMonth
+			},
+			year() {
+				return this.$store.state.currentYear
+			},
+			formattedDate() {
+				return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('YYYY MMMM')
+			}
+		}
+	}
+</script>
