f4edae5e3b4024a14844f07824bad4f9854557e5
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-02-14 12:25:45 +0000

    Lesson 139

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,8 +1,20 @@
 <template>
-	<div>{{ formattedDate }}</div>
+	<div>
+		<div>{{ formattedDate }}</div>
+		<button @click="dec">-</button>
+		<button @click="inc">+</button>
+	</div>
 </template>
 <script>
 	export default {
+		methods: {
+			dec() {
+				console.log('dec')
+			},
+			inc() {
+				console.log('inc')
+			}
+		},
 		computed: {
 			month() {
 				return this.$store.state.currentMonth
