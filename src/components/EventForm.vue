b88edbfb1b8c1b2208cd72db8f0f7eb1a452001e
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-04-04 16:15:07 +0100

    Lesson 153

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
 	<div id="event-form" :class="{ active }" :style="{ top, left }">
 		<h4>Add an Event</h4>
 		<div class="text">
-			<input type="text" v-model="description">
+			<input type="text" v-model="description" placeholder="Boardgame Night">
 		</div>
 		<div class="buttons">
 			<button @click="create">Create</button>
@@ -22,7 +22,11 @@
 				this.$store.commit('eventFormActive', false)
 			},
 			create() {
-				this.$store.commit('addEvent', this.description)
+				if(this.description.length > 0) {
+					this.$store.commit('addEvent', this.description)
+					this.description = ''
+					this.$store.commit('eventFormActive', false)
+				}
 			}
 		},
 		computed: {
