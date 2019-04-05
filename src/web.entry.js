a2a1e02fb610748861adf5cc571918dbf9aa94e1
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-04-05 15:01:00 +0100

    Lesson 169

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,13 +1,7 @@
-import Vue from 'vue'
 import './style.scss'
-
-import store from './store'
 
 import moment from 'moment-timezone'
 moment.tz.setDefault('UTC')
-Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
-
-import App from './components/App.vue'
 
 let events = window.__INITIAL_STATE__.map(event =>{
 	return {
@@ -15,27 +9,7 @@ let events = window.__INITIAL_STATE__.map(event =>{
 		date: moment(event.date)
 	}
 })
-let initialState = Object.assign({}, store.state, { events })
-store.replaceState(initialState)
 
-new Vue({
-  el: '#app',
-  data: {
-  	moment
-  },
-  components: {
-  	App
-  },
-  store,
-  render(createElement) {
-  	return createElement(
-  		'div',
-  		{
-  			attrs: { id: 'app' }
-  		},
-  		[
-  			createElement('app')
-  		]
-  	)
-  }
-});
+import VueCalendar from './entry'
+
+VueCalendar(events).$mount('#app')
