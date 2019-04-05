68ad589a90c530091ec6123daf1bec792a5aff8b
Jorge Fernandes <jorge.fernandes@timewax.com>
2019-04-05 11:58:08 +0100

    Lesson 166

--- src/web.entry.js
+++ src/web.entry.js
@@ -26,5 +26,16 @@ new Vue({
   components: {
   	App
   },
-  store
+  store,
+  render(createElement) {
+  	return createElement(
+  		'div',
+  		{
+  			attrs: { id: 'app' }
+  		},
+  		[
+  			createElement('app')
+  		]
+  	)
+  }
 });
